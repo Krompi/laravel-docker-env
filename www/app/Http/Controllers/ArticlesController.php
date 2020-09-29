e8b137239807c1a2ae5d5e51b6752ead08abc069
krompi <matthias.krompass@adbv-a.bayern.de>
2020-09-29 11:11:22 +0200

    Episode 21

--- www/app/Http/Controllers/ArticlesController.php
+++ www/app/Http/Controllers/ArticlesController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
+    // Render a list of as resource
     public function index()
     {
         $articles = $articles = Article::latest()->get();
@@ -15,10 +16,37 @@ class ArticlesController extends Controller
 
         return view('articles.index', ['articles' => $articles]);
     }
+
+    // show a single resource
     public function show($id)
     {
         $article = Article::find($id);
         
         return view('articles.show', ['article' => $article]);
+    }
+    
+    // shows a view to create a new resource
+    public function create($id)
+    {
+    }
+    
+    // persist the new resource
+    public function store($id)
+    {
+    }
+    
+    // show a view to edit an existing resource
+    public function edit($id)
+    {
+    }
+    
+    // persist the edited resource
+    public function update($id)
+    {
+    }
+    
+    // delete the resource
+    public function destroy($id)
+    {
     }
 }
