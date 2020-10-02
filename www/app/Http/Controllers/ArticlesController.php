9f5ead44963d1c7db451067d7cc3d7f530a087c3
krompi <matthias.krompass@adbv-a.bayern.de>
2020-10-02 13:28:54 +0200

    Episode 24

--- www/app/Http/Controllers/ArticlesController.php
+++ www/app/Http/Controllers/ArticlesController.php
@@ -45,11 +45,19 @@ class ArticlesController extends Controller
     // show a view to edit an existing resource
     public function edit($id)
     {
+        $article = Article::find($id);
+        return view('articles.edit', compact('article'));
     }
     
     // persist the edited resource
     public function update($id)
     {
+        $article = Article::find($id);
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+        $article->save();
+        return redirect('/articles/' . $article->id);
     }
     
     // delete the resource
